1dc0a2012a7ce1a35d9d11cb5d560c6a72d9ff37
mochineko <mochinekoo0113@gmail.com>
2026-02-11 10:20:24 +0900

    [add]　イージング関数を追加しました

--- MathUtil.h
+++ MathUtil.h
@@ -6,5 +6,23 @@ namespace MathUtilty {
 
 	const float PI = 3.1415926535f;
 
+	namespace EaseFunction {
 
+		inline float easeInSine(float x) {
+			return 1 - cos((x * PI) / 2);
+		}
+
+		inline float easeOutSine(float x) {
+			return sin((x * PI) / 2);
+		}
+
+		inline float GetLerp(int start, int end, float rate, float (*easeFunc)(float x) = nullptr) {
+			if (rate < 0.0f) rate = 0.0f;
+			if (rate > 1.0f) rate = 1.0f;
+			if (easeFunc != nullptr) {
+				rate = easeFunc(rate);
+			}
+			return (end - start) * rate + start;
+		}
+	}
 }
